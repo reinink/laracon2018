614b08c7bf1b07447029cb6d1ea602926802bb7c
Jonathan Reinink <jonathan@reinink.ca>
2018-02-06 20:55:15 -0500

    Add last interaction date

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -9,12 +9,14 @@
         <th>Name</th>
         <th>Company</th>
         <th>Birthday</th>
+        <th>Last Interaction</th>
     </tr>
     @foreach ($customers as $customer)
         <tr>
             <td><a href="{{ route('customers.edit', $customer) }}">{{ $customer->last_name }}, {{ $customer->first_name }}</a></td>
             <td>{{ $customer->company->name }}</td>
             <td>{{ $customer->birth_date->format('F j') }}</td>
+            <td>{{ $customer->interactions->sortByDesc('created_at')->first()->created_at->diffForHumans() }}</td>
         </tr>
     @endforeach
 </table>
