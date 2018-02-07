8c013f8b9f55464837a8b1d56b1efeb611f6c35e
Jonathan Reinink <jonathan@reinink.ca>
2018-02-06 20:54:03 -0500

    Add company name

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -7,11 +7,13 @@
 <table class="table my-4">
     <tr>
         <th>Name</th>
+        <th>Company</th>
         <th>Birthday</th>
     </tr>
     @foreach ($customers as $customer)
         <tr>
             <td><a href="{{ route('customers.edit', $customer) }}">{{ $customer->last_name }}, {{ $customer->first_name }}</a></td>
+            <td>{{ $customer->company->name }}</td>
             <td>{{ $customer->birth_date->format('F j') }}</td>
         </tr>
     @endforeach
