d179545774ea2e43b657bfc88c89a3c6650ad739
Jonathan Reinink <jonathan@reinink.ca>
2018-02-06 20:54:20 -0500

    Eager load companies

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,7 +9,7 @@ class CustomersController extends Controller
 {
     public function index(Request $request)
     {
-        $customers = Customer::orderByName()->paginate();
+        $customers = Customer::with('company')->orderByName()->paginate();
 
         return view('customers', ['customers' => $customers]);
     }
